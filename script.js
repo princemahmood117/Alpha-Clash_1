58259d6b7f468545ca3b0410f18757aa097ecc53
princemahmood117 <princemahmood117@gmail.com>
2024-02-17 14:13:25 +0600

    first part done

--- script.js
+++ script.js
@@ -14,9 +14,43 @@
     utility_function.js file
 */ 
 
+
+
+
 function play(){
     hideElementByID('home');
     showElementById('play-ground');
-       
+    gameOn();
+
+    // continueGame();
 }
+
+
+// function continueGame(){
+//     step 1: generate a random alphabet  -----> using external function here. (can do it in here as well)
+//     step 2: call the function of random alphabet
+
+//     const alphabet = getRandomAlpha();
+//     console.log("Random alphabet it ",alphabet);
+//     }
+
+
+function gameOn(){
+    const alphabet = getRandomAlphabet();
+    const currentAlpha = document.getElementById('current_alphabet');
+    currentAlpha.innerText = alphabet;
+
+    
+    // setting background color
+
+    setBackgroundColor(alphabet);
+    
+}
+
+
+
+
+
+
+
 
--- utility_function.js
+++ utility_function.js
@@ -8,3 +8,48 @@ function showElementById(elementID){
     element.classList.remove('hidden');
 }
 
+
+           // geting random alphabet
+
+function getRandomAlphabet(){
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabets = alphabetString.split('');
+
+    const randomNum = Math.random()*25;
+    const index = Math.round(randomNum);   
+
+    const alphabet = alphabets[index];
+    return alphabet;
+     
+}
+
+            // adding background color to the key
+
+ function setBackgroundColor(elementID){
+    const element = document.getElementById(elementID);
+    element.classList.add('bg-orange-500');
+            
+            }
+
+
+
+// function getRandomAlpha(){
+//     // first create an alphabet array
+
+//     const alphabetString = "abcdefghijklmnopqrstuvwxyz";  // also can use an array of each alphabet
+//     const alphabets = alphabetString.split('');
+//     // console.log(alphabets);
+
+//     // secondly generate a random index between 0 to 25 (length-1) 
+
+//     const randomNum = Math.random()*25;
+//     const index = Math.round(randomNum);   // Math.round() means the random number will be integer
+//     // console.log(index);
+
+//     const alphabet = alphabets[index];
+//     // console.log(index,alphabet);
+//     return alphabet;
+// }
+
+
+
